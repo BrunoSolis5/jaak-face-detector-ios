37b96ce6e4e9883b7f61e2f54c0e05515208124d
BrunoSolis5 <diego.bruno@jaak.ai>
2025-07-09 13:42:53 -0600

    Initial Commit

---
+++ JAAKFaceDetectorTests/JAAKFaceDetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  JAAKFaceDetectorTests.swift
+//  JAAKFaceDetectorTests
+//
+//  Created by Diego Roman Bruno Solis on 09/07/25.
+//
+
+import Testing
+@testable import JAAKFaceDetector
+
+struct JAAKFaceDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
